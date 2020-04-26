159fc2306a31234772a7e5a9160f9fb34da277e0
saurabhglaucuslogistics <saurabh.tiwari@glaucuslogistics.com>
2020-04-25 23:20:25 -0700

    First Commit

---
+++ CriteriaProject/src/main/java/com/example/demo/CriteriaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriteriaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriteriaProjectApplication.class, args);
+	}
+
+}
